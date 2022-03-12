9c37351525f6d933f084845ae044caddfccc935c
Brian Elliott <ellio2bj@dukes.jmu.edu>
2022-03-12 17:19:10 -0500

    wrapping up the UI

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
-buttons = document.querySelectorAll(".choice");
-restartButton = document.querySelector(".restart")
-playerScore = document.querySelector(".player-score");
-computerScore = document.querySelector(".computer-score");
-statusText = document.querySelector(".status-text");
-statusRounds = document.querySelector(".status-rounds");
-resultWinner = document.querySelector(".result-winner");
+let buttons = document.querySelectorAll(".choice");
+let restartButton = document.querySelector(".restart")
+let playerScore = document.querySelector(".player-score");
+let computerScore = document.querySelector(".computer-score");
+let statusText = document.querySelector(".status-text");
+let statusRounds = document.querySelector(".status-rounds");
+let resultWinner = document.querySelector(".result-winner");
+let roundResults = document.querySelector(".round-results");
 
 let playerWins = 0;
 let computerWins = 0;
@@ -45,6 +46,9 @@ function comparePlayer(playerChoice, computerChoice) {
 function game(playerChoice) {
     let computerChoice = computerPlay();
     let result = comparePlayer(playerChoice, computerChoice);
+
+    removeAllChildElements(roundResults);
+    generateRoundResult(playerChoice, computerChoice, roundResults);
     
     if (result.includes("win")) {
         playerWins++;
@@ -74,6 +78,60 @@ function resetGame() {
         button.classList.add("hidden");
         restartButton.classList.remove("hidden");
     })
+}
+
+function removeAllChildElements(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+function generateRoundResult(playerChoice, computerChoice, element) {
+    let playerImage = document.createElement("img");
+    let computerImage = document.createElement("img");
+    let playerText = document.createElement("p");
+    let computerText = document.createElement("p");
+    let playerDiv = document.createElement("div");
+    let computerDiv = document.createElement("div");
+    let vsText = document.createElement("p");
+
+    playerImage.src = getImage(playerChoice.toLowerCase());
+    computerImage.src = getImage(computerChoice.toLowerCase());
+    playerText.textContent = "You chose: ";
+    computerText.textContent = "Computer chose: ";
+
+    computerDiv.classList.add("col");
+    playerDiv.classList.add("col");
+    element.classList.add("row");
+
+    element.style.justifyContent = "center";
+    element.style.gap = "32px";
+    element.style.margin="32px";
+    playerImage.style.width = "100px";
+    computerImage.style.width = "100px";
+    vsText.innerHTML = "VS";
+
+    element.appendChild(playerDiv);
+    element.appendChild(vsText);
+    element.appendChild(computerDiv);
+    playerDiv.appendChild(playerText);
+    computerDiv.appendChild(computerText);
+    playerDiv.appendChild(playerImage);
+    computerDiv.appendChild(computerImage);
+}
+
+function getImage(choice) {
+    let scissorsImage = "./images/scissors.png";
+    let rockImage = "./images/rock.png";
+    let paperImage = "./images/paper.png";
+
+    if (choice == "rock") {
+        return rockImage;
+    } else if (choice == "paper") {
+        return paperImage;
+    } else {
+        return scissorsImage;
+    }
 }
 
 buttons.forEach((button) => {
